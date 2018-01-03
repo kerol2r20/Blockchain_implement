ca72cfb1ccf9206d048cd202925dcb8188621d14
Yu-Hsin Lu <kerol2r20@gmail.com>
2018-01-03 17:35:37 +0800

    complete basic block

---
+++ src/Block.ts
@@ -0,0 +1,57 @@
+import crypto = require("crypto");
+
+export class Block {
+    private blockNumber: number;
+    private previousHash: string;
+    private timestamp: Date;
+    private data: string;
+    private hash: string;
+    private nonce: number;
+    constructor(blockNumber: number, previousHash: string, data: any = "") {
+        this.blockNumber = blockNumber;
+        this.previousHash = previousHash;
+        this.timestamp = new Date();
+        this.data = data;
+        this.nonce = 0;
+        this.hash = this.doubleSha256();
+    }
+    public mining(difficult: number): void {
+        let pat = "";
+        for (let i = 0; i < difficult; i++) {
+            pat += "0";
+        }
+        for (let i = 0; i < 64 - difficult; i++) {
+            pat += "f";
+        }
+        let newHash = this.hash;
+        while (newHash > pat) {
+            this.nonce += 1;
+            newHash = this.doubleSha256();
+        }
+        this.hash = newHash;
+    }
+    public doubleSha256(): string {
+        const secret = "f30a5884d9d364849d7ba0f64c102785426cbaa866dfd97101c8fdaeff0637cd";
+        const firstResult = crypto
+            .createHmac("sha256", secret)
+            .update(this.serialization())
+            .digest("hex");
+        const secondResult = crypto
+            .createHmac("sha256", secret)
+            .update(firstResult)
+            .digest("hex");
+        return secondResult;
+    }
+    private serialization(): string {
+        const obj = {
+            blockNumber: this.blockNumber,
+            previousHash: this.previousHash,
+            timestamp: this.timestamp.toUTCString(),
+            data: this.data,
+            nonce: this.nonce,
+        };
+        return JSON.stringify(obj);
+    }
+}
+
+export default Block;
